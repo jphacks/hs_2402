125a94097181643c3beebc65a10c2ccf24d1cf46
yuki035 <yukiyuki1918@icloud.com>
2024-10-23 13:12:16 +0900

    Create project

---
+++ Tripper/TripperApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripperApp.swift
+//  Tripper
+//
+//  Created by 森祐樹 on 2024/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TripperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
